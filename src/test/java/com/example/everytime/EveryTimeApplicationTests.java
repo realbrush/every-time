a3621d28f6b602e494bd21d8fdde1aaa0e2cc985
onyoo <dhsdb02@gmail.com>
2023-01-28 23:43:04 +0900

    Initial commit

---
+++ src/test/java/com/example/everytime/EveryTimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.everytime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EveryTimeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
