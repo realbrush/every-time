22913451aa05100bc769105c72f771d9da7472fa
onyoo <dhsdb02@gmail.com>
2023-02-27 11:14:59 +0900

    feat(post): Update dto validate

--- src/main/java/com/example/everytime/dto/PostResponseDto.java
+++ src/main/java/com/example/everytime/dto/PostResponseDto.java
@@ -2,22 +2,27 @@ package com.example.everytime.dto;
 
 import com.example.everytime.domain.posts.Post;
 import lombok.Getter;
+
+import javax.validation.constraints.NotEmpty;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Getter
 public class PostResponseDto {
 
+    @NotEmpty(message = "post id must not be empty")
     private UUID uuid;
+    @NotEmpty(message = "title must not be empty")
     private String title;
-    private String content;
+    @NotEmpty(message = "contents must not be empty")
+    private String contents;
     private int goods;
     private LocalDateTime upload_date;
 
     public PostResponseDto(Post entity){
         this.uuid = entity.getUuid();
         this.title = entity.getTitle();
-        this.content = entity.getContents();
+        this.contents = entity.getContents();
         this.goods = entity.getGoods();
         this.upload_date = entity.getUpload_date();
 
