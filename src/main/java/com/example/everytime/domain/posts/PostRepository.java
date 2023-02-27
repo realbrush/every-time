3a800bee5a98be5812b1f66f4b967e2cc4c04e47
onyoo <dhsdb02@gmail.com>
2023-02-27 11:25:45 +0900

    feat(post): Update findByUuid return type

--- src/main/java/com/example/everytime/domain/posts/PostRepository.java
+++ src/main/java/com/example/everytime/domain/posts/PostRepository.java
@@ -3,6 +3,7 @@ package com.example.everytime.domain.posts;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.swing.text.html.Option;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -10,6 +11,6 @@ import java.util.UUID;
 @Repository
 public interface PostRepository extends JpaRepository<Post, Integer> {
     List<Post> findByTitle(String title);
-    List<Post> findByUuid(UUID uuid);
+    Optional<Post> findByUuid(UUID uuid);
     void deleteByUuid(UUID uuid);
 }
