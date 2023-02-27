3c244471341f8cadd724fd2a74a11d4b355be580
onyoo <dhsdb02@gmail.com>
2023-02-27 11:37:17 +0900

    feat(post): Modify get one post return type

--- src/main/java/com/example/everytime/service/PostService.java
+++ src/main/java/com/example/everytime/service/PostService.java
@@ -3,20 +3,18 @@ package com.example.everytime.service;
 import com.example.everytime.domain.posts.Post;
 import com.example.everytime.dto.PostCreateRequestDto;
 import com.example.everytime.dto.PostResponseDto;
-import com.example.everytime.dto.PostSaveRequestDto;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 @Service
 public interface PostService {
     List<PostResponseDto> getPost(String title); // return title match post
-    Optional<PostResponseDto> getOnePost(UUID uuid);
+    PostResponseDto getOnePost(UUID uuid);
     List<PostResponseDto> getPostList(); // return all post
-    UUID createPost(PostSaveRequestDto item); // return create post
-    Optional<PostResponseDto> updatePost(Post post); // return updated post
+    UUID createPost(PostCreateRequestDto item); // return create post
+    PostResponseDto updatePost(UUID uuid,Post post); // return updated post
     void deletePost(UUID uuid); // delete post by uuid
 
 }
