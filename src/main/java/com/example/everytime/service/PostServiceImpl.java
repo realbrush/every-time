08ae193da70a293a73a72eb250fdbbc6cf9edd45
onyoo <dhsdb02@gmail.com>
2023-02-27 11:39:54 +0900

    feat(post): Add error handling

--- src/main/java/com/example/everytime/service/PostServiceImpl.java
+++ src/main/java/com/example/everytime/service/PostServiceImpl.java
@@ -2,16 +2,14 @@ package com.example.everytime.service;
 
 import com.example.everytime.domain.posts.Post;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
+import java.util.*;
 import java.util.stream.Collectors;
 
 import com.example.everytime.domain.posts.PostRepository;
+import com.example.everytime.dto.PostCreateRequestDto;
 import com.example.everytime.dto.PostResponseDto;
-import com.example.everytime.dto.PostSaveRequestDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -60,57 +58,55 @@ public class PostServiceImpl implements PostService {
     }
 
     @Override
-    public Optional<PostResponseDto> getOnePost(UUID uuid){
+    public PostResponseDto getOnePost(UUID uuid){
         /**
          uuid가 일치하는 게시글을 가져오는 메서드
          @param uuid
          @return PostResponseDto
          */
-        List<Post> posts = postRepository.findByUuid(uuid); //중복은 없음
-        Optional<PostResponseDto> response = null;
-        if(posts.size() == 1){
-            response = Optional.of(new PostResponseDto(posts.get(0)));
-        }
+        Post post = postRepository.findByUuid(uuid).orElseThrow(()-> new IllegalArgumentException("해당 게시글이 없습니다 uuid="+uuid)); //중복은 없음
+        PostResponseDto response = new PostResponseDto(post);
         return response;
     }
 
 
     @Override
     @Transactional
-    public UUID createPost(PostSaveRequestDto item) {
+    public UUID createPost(PostCreateRequestDto item) {
         /**
             새로운 게시글을 만드는 메서드
-            @param PostSaveRequestDto
+            @param PostCreateRequestDto
             @return UUID (생성한 문서의 UUID)
          */
+        try{
+            return postRepository.save(item.toEntity()).getUuid();
+        }catch (DataAccessException e){
+            throw new PostCreationException("게시글 생성에 실패하였습니다.");
+        }
 
-        /*
-        * @todo 게시글 생성 성공,실패 로직 작성하기
-        * @body 성공일 경우 성공 여부,실패일 경우 실패했다는 에러메세지를 리턴해야함
-        * */
-
-        return postRepository.save(item.toEntity()).getUuid();
     }
 
     @Override
     @Transactional
-    public Optional<PostResponseDto> updatePost(Post item) {
+    public PostResponseDto updatePost(UUID uuid,Post request) {
         /**
          * 게시글을 업데이트 하는 메서드
          * @param PostUpdateRequestDto
          * @return
          * */
+        Post post = postRepository.findByUuid(uuid)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. uuid=" + uuid));
 
-        /*
-         * @todo dto update dto 형식에 맞게 구성하기
-         * @body dto 형식에 맞게 다시짜고 업데이트 해주기
-         */
-        List<Post> posts = postRepository.findByUuid(item.getUuid()); //중복은 없음
-        Optional<PostResponseDto> response = null;
-        if(posts.size() == 1){
-            response = Optional.of(new PostResponseDto(posts.get(0)));
+        // title 또는 contents 중 적어도 하나가 null이 아닌 경우에만 업데이트를 진행함
+        if (request.getTitle() != null || request.getContents() != null) {
+            String title = Optional.ofNullable(request.getTitle()).orElse(post.getTitle());
+            String contents = Optional.ofNullable(request.getContents()).orElse(post.getContents());
+            post.update(title, contents);
+        } else {
+            throw new IllegalArgumentException("제목 또는 내용 중 적어도 하나는 반드시 입력되어야 합니다.");
         }
-        return response;
+
+        return new PostResponseDto(postRepository.save(post));
     }
 
     @Override
@@ -120,8 +116,19 @@ public class PostServiceImpl implements PostService {
         *  @todo 게시글 삭제 로직 구현하기
         *  @body post 삭제여부 확인한뒤, 삭제 할 게시글이 없으면 에러메세지를 출력하는 로직 작성하기
         * */
-        postRepository.deleteByUuid(uuid);
+        Post post = postRepository.findByUuid(uuid)
+                .orElseThrow(() -> new NoSuchElementException("해당 게시글이 없습니다. uuid=" + uuid));
+
+        postRepository.delete(post);
+
+    }
+    public class PostCreationException extends RuntimeException {
+        public PostCreationException(String message) {
+            super(message);
+        }
     }
 
 }
+
+
 
