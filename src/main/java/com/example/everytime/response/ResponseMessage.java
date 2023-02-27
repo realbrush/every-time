52b75047fa99320f321ebd7895bfd419e45d23d2
onyoo <dhsdb02@gmail.com>
2023-02-27 11:27:06 +0900

    feat(post): Add response message

---
+++ src/main/java/com/example/everytime/response/ResponseMessage.java
@@ -0,0 +1,11 @@
+package com.example.everytime.response;
+
+public class ResponseMessage {
+    public static final String CREATED_POST = "게시글 생성";
+    public static final String UPDATE_POST = "게시글 업데이트";
+    public static final String DELETE_POST = "게시글 삭제";
+    public static final String DELETE_POST_FAILED = "삭제할 게시글이 없습니다";
+    public static final String READ_POST = "게시글 조회 성공";
+    public static final String NO_POST = "작성된 게시글이 없습니다";
+    public static final String FAIL_DEFAUL_RES = "요청 실패";
+}
