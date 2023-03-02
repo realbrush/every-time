436b2e8105ef5688a642cbba1db01849917b86c4
onyoo <dhsdb02@gmail.com>
2023-03-02 14:13:00 +0900

    feat(post): Modify find method

--- src/main/java/com/example/everytime/domain/posts/PostRepository.java
+++ src/main/java/com/example/everytime/domain/posts/PostRepository.java
@@ -3,14 +3,15 @@ package com.example.everytime.domain.posts;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import javax.swing.text.html.Option;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Integer> {
+    List<Post> findByIsDeleted(boolean is_deleted);
     List<Post> findByTitle(String title);
+    List<Post> findByTitleAndIsDeleted(String title,boolean is_deleted);
     Optional<Post> findByUuid(UUID uuid);
     void deleteByUuid(UUID uuid);
 }
