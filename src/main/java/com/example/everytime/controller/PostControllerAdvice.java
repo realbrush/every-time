2b8d124bbca163e1e484be4c84f22e88fb3d6c0a
onyoo <dhsdb02@gmail.com>
2023-03-02 14:11:00 +0900

    feat(post): Add post not found exception catcher

--- src/main/java/com/example/everytime/controller/PostControllerAdvice.java
+++ src/main/java/com/example/everytime/controller/PostControllerAdvice.java
@@ -1,14 +1,21 @@
 package com.example.everytime.controller;
 
 import com.example.everytime.service.PostServiceImpl;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class PostControllerAdvice {
+    //service error catch
     @ExceptionHandler(PostServiceImpl.PostCreationException.class)
     public ResponseEntity<String> handlePostCreationException(PostServiceImpl.PostCreationException e) {
         return ResponseEntity.badRequest().body(e.getMessage());
+    }
+
+    @ExceptionHandler(PostServiceImpl.PostNotFoundException.class)
+    public ResponseEntity<String> handlePostNotFoundException(PostServiceImpl.PostNotFoundException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
     }
 }
