12b71330ecca24056790d0351a4730f43f1529f8
onyoo <dhsdb02@gmail.com>
2023-04-20 20:46:12 +0900

    feat(login): Add session login filter config #8

---
+++ src/main/java/com/example/everytime/config/WebMvcConfig.java
@@ -0,0 +1,16 @@
+package com.example.everytime.config;
+
+import com.example.everytime.interceptor.SessionCheckInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry){
+        registry.addInterceptor(new SessionCheckInterceptor())
+                .excludePathPatterns("/users/login");
+    }
+}
