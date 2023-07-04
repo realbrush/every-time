45fabe00907e84e7059d056bbf2c4865b8078002
onyoo <dhsdb02@gmail.com>
2023-07-04 11:19:09 +0900

    feat(post): Add original post dto #20

---
+++ src/main/java/com/example/everytime/dto/post/PostDto.java
@@ -0,0 +1,32 @@
+package com.example.everytime.dto.post;
+
+import com.example.everytime.domain.posts.Post;
+import lombok.Getter;
+
+import javax.validation.constraints.NotEmpty;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+public class PostDto {
+    @NotEmpty(message = "post id must not be empty")
+    private UUID uuid;
+
+    @NotEmpty(message = "title must not be empty")
+    private String title;
+
+    @NotEmpty(message = "contents must not be empty")
+    private String contents;
+
+    private int goods;
+
+    private LocalDateTime upload_date;
+
+    public PostDto(Post entity){
+        this.uuid = entity.getUuid();
+        this.title = entity.getTitle();
+        this.contents = entity.getContents();
+        this.goods = entity.getGoods();
+        this.upload_date = entity.getUpload_date();
+    }
+}
